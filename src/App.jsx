e5b8a6799144c99f7f2b8a266100eec8a1bcf42f
Abhishek Patil <abishkpatil@gmail.com>
2024-05-15 19:35:50 +0530

    feat: add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 // import './App.css'
 import Tagline from './components/Tagline/Tagline'
 import Home from './pages/Home'
+import Navbar from './components/Navbar/Navbar'
 function App() {
   return (
     <>
       <Tagline />
+      <Navbar />
       <Home />
     </>
   )
