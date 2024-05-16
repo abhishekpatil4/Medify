13f40909bbc5d0cf89c55d45eedd57e58e2b304c
Abhishek Patil <abishkpatil@gmail.com>
2024-05-16 18:58:27 +0530

    feat: add offers carousel using slick library

--- src/components/Offers/Offers.jsx
+++ src/components/Offers/Offers.jsx
@@ -1,13 +1,10 @@
-import { autocompleteClasses, requirePropFactory } from "@mui/material";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import 'swiper/css';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Offer from "./Offer";
 import OfferImage1 from "../../assets/Healthcare Consultation-1.png"
 import OfferImage2 from "../../assets/Healthcare Consultation-2.png"
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 
 const data = [
     {
@@ -29,32 +26,48 @@ const data = [
     {
         id: 5,
         path: OfferImage1
-    },
-    {
-        id: 6,
-        path: OfferImage2
-    },
+    }
 ]
 
-const Offers = () => {
-    return <div style={{transform: 'translate(0, -10%)', padding: '10rem 5rem 5rem 5rem', backgroundColor: 'white'}}>
-            <Swiper
-                modules={[Navigation, Pagination, Scrollbar]}
-                // spaceBetween={10} 
-                // navigation
-                pagination
-                slidesPerView={"auto"}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-            >
 
-                {
-                    data && data.map((d) =>
-                        <SwiperSlide key={d.id} style={{ maxWidth: '399px' }}><Offer image={d.path}/></SwiperSlide>
-                    )
+function Offers() {
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1350,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
                 }
-            </Swiper>
+            },
+            {
+                breakpoint: 960,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    };
+    return (
+        <div style={{ transform: 'translate(0, -10%)', padding: '10rem 5rem 5rem 5rem', backgroundColor: 'white' }}>
+            <div className="slider-container">
+                <Slider {...settings}>
+                    {
+                        data && data.map((d) =>
+                            <Offer image={d.path} />
+                        )
+                    }
+                </Slider>
+            </div>
         </div>
+    );
 }
 
 export default Offers;
