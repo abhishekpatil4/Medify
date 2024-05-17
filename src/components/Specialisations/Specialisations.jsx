773a62d486f19d273bf54410ee6825a98fae47e8
Abhishek Patil <abishkpatil@gmail.com>
2024-05-17 18:35:10 +0530

    fix: add key to Grid comp

--- src/components/Specialisations/Specialisations.jsx
+++ src/components/Specialisations/Specialisations.jsx
@@ -49,7 +49,7 @@ const Specialisations = () => {
             <Grid container rowSpacing={5}>
                 {
                     data && data.map((d) =>
-                        <Grid item xs={12} sm={6} md={4} lg={3} style={{display:'flex', justifyContent:'center'}}>
+                        <Grid key={d.id} item xs={12} sm={6} md={4} lg={3} style={{display:'flex', justifyContent:'center'}}>
                             <Specialisation name={d.name} />
                         </Grid>
                     )
