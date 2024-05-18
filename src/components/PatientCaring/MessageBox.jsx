c051f18ea8037ee602195db0b03a38d5cf7f63ed
Abhishek Patil <abishkpatil@gmail.com>
2024-05-19 01:25:09 +0530

    fix: minor bugs

--- src/components/PatientCaring/MessageBox.jsx
+++ src/components/PatientCaring/MessageBox.jsx
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 import { useTheme } from "@mui/material";
 const Message = () => {
     const theme = useTheme();
-    return <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', position: 'absolute', height: '86px', top: '100px', left: '122px', width: '234px', display: 'flex', zIndex: 1, backgroundColor: 'white', borderRadius: '8px', boxShadow: "0px 15px 25px 0px #0000000F" }}>
+    return <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', position: 'absolute', height: '86px', top: '95px', left: '70px', width: '234px', display: 'flex', zIndex: 1, backgroundColor: 'white', borderRadius: '8px', boxShadow: "0px 15px 25px 0px #0000000F" }}>
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 5 }}>
             <img src={mobile} alt="Smiley face" width="43px" height="34px" style={{ backgroundColor: 'white' }} />
             <Typography style={{ fontSize: '16px', lineHeight: '27px', fontWeight: 700, color: theme.palette.secondary.main }}>Free Consultation</Typography>
--- src/components/PatientCaring/PatientCaring.jsx
+++ src/components/PatientCaring/PatientCaring.jsx
@@ -3,34 +3,32 @@ import photo1 from "../../assets/patientCaring/patientCaring1.png";
 import photo2 from "../../assets/patientCaring/patientCaring2.png";
 import { Typography } from "@mui/material";
 import { useTheme } from "@mui/material";
-import mobile from "../../../src/assets/mobileIcon/main.svg"
 import CustomListItem from "./CustomListItem";
+import Message from "./MessageBox";
 
 const PatientCaring = () => {
     const theme = useTheme();
     return <div style={{ display: 'flex', flexDirection: 'row', paddingBottom: '5rem', justifyContent: 'space-evenly', alignItems: 'center' }}>
-        <div style={{ position: 'relative', width: '519px', height: '486px', borderRadius: '8px 0px 0px 0px' }}>
-            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', position: 'absolute', height: '86px', top: '100px', left: '122px', width: '234px', display: 'flex', zIndex: 1, backgroundColor: 'white', borderRadius: '8px', boxShadow: "0px 15px 25px 0px #0000000F" }}>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 5 }}>
-                    <img src={mobile} alt="Smiley face" width="43px" height="34px" style={{ backgroundColor: 'white' }} />
-                    <Typography style={{ fontSize: '16px', lineHeight: '27px', fontWeight: 700, color: theme.palette.secondary.main }}>Free Consultation</Typography>
-                </div>
-                <Typography style={{ textAlign: 'center', fontSize: '15px', lineHeight: '24px', fontWeight: 500, color: 'gray' }}>Consultation with the best</Typography>
+        <div style={{ width: '519px', height: '486px', borderRadius: '8px 0px 0px 0px' }}>
+            <div style={{ position: 'relative' }}>
+                <Message />
             </div>
-            <img src={photo1} alt="doctor speaking to a patient" style={{ objectFit: 'cover', position: 'relative', border: "5px solid white", width: "332px", height: "275px", left: '305px', top: '0px' }} />
-            <img src={photo2} alt="doctor speaking to a patient" style={{ objectFit: 'cover', position: 'relative', border: "5px solid white", width: '332px', height: '275px', left: '201px', top: "-75px" }} />
+            <div style={{ position: "relative" }}>
+                <img src={photo1} alt="doctor speaking to a patient" style={{ objectFit: 'cover', position: 'absolute', border: "5px solid white", width: "332px", height: "275px", left: '250px', top: '0px' }} />
+                <img src={photo2} alt="doctor speaking to a patient" style={{ objectFit: 'cover', position: 'absolute', border: "5px solid white", width: '332px', height: '275px', left: '150px', top: "220px" }} />
+            </div>
         </div>
-        <div style={{textAlign: 'left' }}>
-            <Typography style={{ position: 'relative', top: "60px", fontSize: '16px', fontWeight: 600, lineHeight: '27px', color: theme.palette.primary.main }}>
+        <div style={{ textAlign: 'left' }}>
+            <Typography style={{ fontSize: '16px', fontWeight: 600, lineHeight: '27px', color: theme.palette.primary.main }}>
                 HELPING PATIENTS FROM AROUND THE GLOBE!!
             </Typography>
-            <Typography style={{ position: 'relative', top: '65px', fontSize: '48px', fontWeight: 600, lineHeight: '67px', color: theme.palette.primary.main }}>
+            <Typography style={{ fontSize: '48px', fontWeight: 600, lineHeight: '67px', color: theme.palette.primary.main }}>
                 <span style={{ color: theme.palette.secondary.main }}>Patient</span> Caring
             </Typography>
-            <Typography style={{ top: '90px', position: 'relative', height: '87px', width: '563px', color: '#77829D', fontSize: '17px', fontWeight: 500, lineHeight: '29px' }}>
+            <Typography style={{ height: '87px', width: '563px', color: '#77829D', fontSize: '17px', fontWeight: 500, lineHeight: '29px', margin: '1.5rem 0rem' }}>
                 Our goal is to deliver quality of care in a courteous, respectful, and compassionate manner. We hope you will allow us to care for you and strive to be the first and best choice for healthcare.
             </Typography>
-            <div style={{ position: "relative", top: '120px' }}>
+            <div style={{}}>
                 <CustomListItem textContent={"Stay Updated About Your Health"} />
                 <CustomListItem textContent={"Check Your Results Online"} />
                 <CustomListItem textContent={"Manage Your Appointments"} />
