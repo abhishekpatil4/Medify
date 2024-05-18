7613440945145475375c82b51596c7e9d6b04a84
Abhishek Patil <abishkpatil@gmail.com>
2024-05-18 19:47:19 +0530

    feat: add Smiley face and happy patient count to FAQ section

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -7,7 +7,7 @@ import heartInHand from "../../assets/FAQ/heartInHand.svg"
 
 const FAQ = () => {
     const theme = useTheme();
-    return <div style={{ backgroundColor: 'white', paddingBottom: '3rem' }}>
+    return <div style={{ backgroundColor: 'white', paddingBottom:"3rem"}}>
         <Container maxWidth="xl" style={{ paddingTop: '2rem' }}>
             <Typography style={{ textAlign: 'center', fontSize: '16px', fontWeight: 600, lineHeight: '27px', color: theme.palette.primary.main }}>
                 Get Your Answer
@@ -16,12 +16,20 @@ const FAQ = () => {
                 Frequently Asked Questions
             </Typography>
             <div style={{ flexWrap: 'wrap', display: 'flex', justifyContent: 'space-evenly', marginTop: '3rem' }}>
-                <div>
-                    <div style={{ position: 'relative', left: '470px', top: '230px', height: '80px', width: '80px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div style={{position:'relative'}}>
+                    <div style={{ position: 'absolute', left: '468px', top: '150px', height: '80px', width: '80px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <img src={heartInHand} alt="Heart in hand icon" width="80px" height="40px" style={{ backgroundColor: 'white', padding: '1rem', borderRadius: '100%', boxShadow: "0px 15px 25px 0px #0000000F" }} />
                     </div>
                     <div style={{ borderRadius: '8px', width: '500px', height: '480px', border: '10px solid #FFFFFF', boxShadow: '0px 15px 55px -10px #00000017' }}>
                         <img width={"500px"} height={"480px"} style={{ objectFit: 'cover' }} src={doctorPatientTalking} alt="Doctor talking to a patient" />
+                    </div>
+                    {/* left: '-110px', top: '-198px', */}
+                    <div style={{ display: 'flex', position: 'absolute' ,height: '104px',top:'300px', left:'-110px', width: '240px', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex:1, backgroundColor:'white', borderRadius: '8px', boxShadow: "0px 15px 25px 0px #0000000F" }}>
+                        <img src={Smiley} alt="Smiley face" width="44px" height="44px" style={{ backgroundColor: 'white', padding: '0rem 1rem 0rem 1rem' }} />
+                        <div>
+                            <Typography style={{ fontSize: '24px', lineHeight: '33px', fontWeight: 600, color: theme.palette.secondary.main }}>84k+</Typography>
+                            <Typography style={{ fontSize: '17px', lineHeight: '28px', fontWeight: 400, color: 'gray' }}>Happy Patients</Typography>
+                        </div>
                     </div>
                 </div>
                 <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly' }}>
