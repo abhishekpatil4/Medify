41cacdac5b109a8aad0566e320f68b29933faa01
Abhishek Patil <abishkpatil@gmail.com>
2024-05-19 00:50:25 +0530

    feat: add Free consultation message on Patient caring component

--- src/components/PatientCaring/PatientCaring.jsx
+++ src/components/PatientCaring/PatientCaring.jsx
@@ -4,11 +4,19 @@ import photo2 from "../../assets/patientCaring/patientCaring2.png";
 import { Typography } from "@mui/material";
 import { useTheme } from "@mui/material";
 import Icon from "../../../src/assets/Healthcare Consultation Clip Path Group.svg"
+import mobile from "../../../src/assets/mobileIcon/main.svg"
 
 const PatientCaring = () => {
     const theme = useTheme();
-    return <div style={{ display: 'flex', flexDirection: 'row', paddingBottom:'5rem' }}>
+    return <div style={{ display: 'flex', flexDirection: 'row', paddingBottom: '5rem' }}>
         <div style={{ position: 'relative', width: '519px', height: '486px', left: '138px', borderRadius: '8px 0px 0px 0px' }}>
+            <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', position: 'absolute', height: '86px', top: '100px', left: '122px', width: '234px', display: 'flex', zIndex: 1, backgroundColor: 'white', borderRadius: '8px', boxShadow: "0px 15px 25px 0px #0000000F" }}>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap:5 }}>
+                    <img src={mobile} alt="Smiley face" width="43px" height="34px" style={{ backgroundColor: 'white' }} />
+                    <Typography style={{ fontSize: '16px', lineHeight: '27px', fontWeight: 700, color: theme.palette.secondary.main }}>Free Consultation</Typography>
+                </div>
+                <Typography style={{textAlign:'center', fontSize: '15px', lineHeight: '24px', fontWeight: 500, color: 'gray' }}>Consultation with the best</Typography>
+            </div>
             <img src={photo1} alt="doctor speaking to a patient" style={{ objectFit: 'cover', position: 'relative', border: "5px solid white", width: "332px", height: "275px", left: '305px', top: '0px' }} />
             <img src={photo2} alt="doctor speaking to a patient" style={{ objectFit: 'cover', position: 'relative', border: "5px solid white", width: '332px', height: '275px', left: '201px', top: "-75px" }} />
         </div>
