c7396d2984a6766343f3099891c07d533f36a5b8
Abhishek Patil <abishkpatil@gmail.com>
2024-05-18 10:09:26 +0530

    feat: add footer responsiveness

--- src/components/Footer/DropDownItem.jsx
+++ src/components/Footer/DropDownItem.jsx
@@ -1,11 +1,12 @@
 import rightIcon from "../../assets/RightIcon.svg"
+import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
 const DropDownItem = ({ name }) => {
-    return <div style={{
-        display: 'flex', alignItems: 'center', gap: 15
-    }}>
+    return <Box sx={{
+        display: 'flex', alignItems: 'center', gap: 2}}>
         <img src={rightIcon} alt="" />
-        <span style={{ fontWeight: 400, fontSize: '16px', color: 'white', lineHeight: '28px' }}>{name}</span>
-    </div>
+        <Typography style={{ fontWeight: 400, fontSize: '16px', color: 'white', lineHeight: '28px' }}>{name}</Typography>
+    </Box>
 }
 
 export default DropDownItem;
