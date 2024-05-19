8ae242cc2fd283179b8d9588d1357885a2f33a7f
Abhishek Patil <abishkpatil@gmail.com>
2024-05-19 11:44:18 +0530

    feat: add API fetch for hospitals

--- src/pages/Hospitals.jsx
+++ src/pages/Hospitals.jsx
@@ -1,7 +1,22 @@
-import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom"
 
-const Hospitals  = () => {
-    let { state, city } = useParams();
+const Hospitals = () => {
+    const [hospitals, setHospitals] = useState();
+    const { state, city } = useParams();
+    useEffect(() => {
+        const getHospitals = async() => {
+            const url = `https://meddata-backend.onrender.com/data?state=${state}&city=${city}`;
+            try {
+                const res = await axios.get(url);
+                console.log("res", res.data);
+            } catch (error) {
+                console.log(error.response);
+            }
+        }
+        getHospitals();
+    }, [])
     return <div>
         <h1>state: {state}</h1>
         <h1>city: {city}</h1>
