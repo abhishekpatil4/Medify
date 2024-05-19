ade019b2a9cd81cf950e34de0e7063aae77b9857
Abhishek Patil <abishkpatil@gmail.com>
2024-05-19 09:08:59 +0530

    feat: added props to contorl Custom Button

--- src/components/Button/CustomButton.jsx
+++ src/components/Button/CustomButton.jsx
@@ -1,11 +1,25 @@
 import { Button, Icon } from "@mui/material";
 import { useTheme } from "@emotion/react";
 
-const CustomButton = ({ type, icon, fun }) => {
+const CustomButton = ({ type, icon, fun, height = '40px', width = '150px', padding= '0px 18px 0px 18px',  margin= '1rem 0rem' }) => {
     const theme = useTheme();
-    return <Button startIcon={icon} onClick={fun} style={{ backgroundColor: theme.palette.primary.main, color: 'white', textTransform: 'none', borderRadius: '8px', paddingLeft: '18px', paddingRight: '18px', margin: '1rem 0rem' }}>
-        {type}
-    </Button>
+    const customStyles = {
+        backgroundColor: theme.palette.primary.main,
+        color: 'white',
+        textTransform: 'none',
+        borderRadius: '8px',
+        padding: padding,
+        margin: margin,
+        height: height, 
+        width: width,
+    };
+
+    return (
+        <Button startIcon={icon} onClick={fun} style={customStyles}>
+            {type}
+        </Button>
+    );
 }
 
 export default CustomButton;
+
