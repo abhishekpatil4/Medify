20f5ea56931b7cff006426e51e4f806dc0321a48
Abhishek Patil <abishkpatil@gmail.com>
2024-05-19 19:29:16 +0530

    feat: add hospital card

---
+++ src/components/HospitalCard/HospitalCard.jsx
@@ -0,0 +1,44 @@
+import { Box } from "@mui/material";
+import Hospitalicon from "../../assets/HospitalPage/Hospitalicon.svg"
+import { Typography } from "@mui/material";
+import { useTheme } from "@emotion/react";
+import FreeIcon from "../../assets/HospitalPage/free.svg";
+import CustomButton from "../Button/CustomButton";
+import thumbsUp from "../../assets/HospitalPage/thumbsup.svg"
+const HospitalCard = () => {
+    const theme = useTheme();
+    return <Box sx={{ backgroundColor: 'white', borderRadius: '15px', width: '775px', height: '268px', display: 'flex', justifyContent: 'space-evenly', flexWrap: 'wrap', padding: '10px', marginTop: '1rem' }}>
+        <img src={Hospitalicon} alt="" height={"140px"} width={"140px"} style={{ marginTop: '1rem' }} />
+        <Box>
+            <Box sx={{ width: '310px', height: '220px', display: 'flex', justifyContent: 'center', alignItems: 'flex-start', flexDirection: 'column', gap: 1, borderBottom: "1px dashed #E8E8F0" }}>
+                <Typography sx={{ color: theme.palette.primary.main, fontSize: '20px', fontWeight: 600, lineHeight: '28px' }}>
+                    Fortis Hospital Richmond Road
+                </Typography>
+                <Typography sx={{ color: '#414146', fontSize: '14px', fontWeight: 700, lineHeight: '20px' }}>
+                    Banglore, Karnataka
+                </Typography>
+                <Typography sx={{ color: '#414146', fontSize: '14px', fontWeight: 400, lineHeight: '20px' }}>
+                    Smilessence Center for Advanced Dentistry + 1 <br /> more
+                </Typography>
+                <Box sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', flexWrap: 'wrap', gap: 1 }}>
+                    <img src={FreeIcon} alt="" />
+                    <Typography sx={{ textDecoration: 'line-through', color: '#787887', fontSize: '13px' }}>
+                        ₹500
+                    </Typography>
+                    <Typography sx={{ color: '#414146', fontSize: '14px', fontWeight: 400, lineHeight: '20px' }}>
+                        Consultation fee at clinic
+                    </Typography>
+                </Box>
+            </Box>
+            <img src={thumbsUp} alt="" style={{marginTop:'15px'}}/>
+        </Box>
+        <Box sx={{ width: '236px', display: 'flex', justifyContent: 'flex-end', alignItems: 'center', flexDirection: 'column' }}>
+            <Typography sx={{ color: '#01A400', fontWeight: 500, fontSize: '14px', lineHeight: '20px' }}>Available Today</Typography>
+            <CustomButton type={"BOOK FREE Center Visit"} width={"14rem"} />
+        </Box>
+    </Box>
+}
+
+
+export default HospitalCard;
+
--- src/pages/Hospitals.jsx
+++ src/pages/Hospitals.jsx
@@ -2,34 +2,64 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom"
 import Navbar from '../components/Navbar/Navbar'
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import SearchComp from "../components/SearchBox/SearchComp";
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import { Container } from "@mui/material";
+import tickMark from "../assets/HospitalPage/tickMark.svg";
+import HospitalCard from "../components/HospitalCard/HospitalCard";
 
 const Hospitals = () => {
     const [hospitals, setHospitals] = useState();
     const { state, city } = useParams();
+    const [searched, setSearched] = useState(false);
     useEffect(() => {
+        console.log(" page rerender");
         const getHospitals = async () => {
             const url = `https://meddata-backend.onrender.com/data?state=${state}&city=${city}`;
             try {
                 const res = await axios.get(url);
+                console.log("res: ", res.data);
                 setHospitals(res.data);
             } catch (error) {
                 console.log(error.response);
             }
         }
+        setSearched(false);
         getHospitals();
-    }, [])
+    }, [searched])
     return <Box>
         <Navbar bgColor={"white"} />
         <Box sx={{ position: 'relative' }}>
             <Box sx={{ background: 'linear-gradient(91.75deg, #2AA7FF 1.4%, #0C8CE6 100.57%)', height: '110px', borderRadius: '0px 0px 16px 16px' }}></Box>
             <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
-                <Box sx={{position: 'absolute', backgroundColor: "white", borderRadius: '15px', boxShadow: '6px 6px 35px 0px #1028511C', width: '1300px' }}>
+                <Box onClick={() => setSearched(true)} sx={{ position: 'absolute', backgroundColor: "white", borderRadius: '15px', boxShadow: '6px 6px 35px 0px #1028511C', width: '1300px' }}>
                     <SearchComp padding={"0.6rem"} />
                 </Box>
             </Box>
         </Box>
+        <Container>
+            <Box sx={{ flexGrow: 1, paddingTop: '5rem' }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={9}>
+                        <Typography sx={{ fontSize: '24px', fontWeight: 500, lineHeight: '36px' }}>
+                            {hospitals ? hospitals.length : "loading..."} medical centers available in {city}
+                        </Typography>
+                        <Box sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', gap:3 }}>
+                            <img src={tickMark} alt="" />
+                            <Typography sx={{ fontSize: '16px', fontWeight: 400, lineHeight: '24px', color: '#787887' }}>
+                                Book appointments with minimum wait-time & verified doctor details
+                            </Typography>
+                        </Box>
+                        <HospitalCard />
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                        hi
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
     </Box>
 }
 
