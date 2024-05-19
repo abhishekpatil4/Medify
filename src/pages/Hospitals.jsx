da13b0e67d6334155270f57da9d0e841c13dae31
Abhishek Patil <abishkpatil@gmail.com>
2024-05-19 12:20:24 +0530

    feat: add searchComp to Hospitals page

--- src/pages/Hospitals.jsx
+++ src/pages/Hospitals.jsx
@@ -3,12 +3,13 @@ import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom"
 import Navbar from '../components/Navbar/Navbar'
 import { Box } from "@mui/material";
+import SearchComp from "../components/SearchBox/SearchComp";
 
 const Hospitals = () => {
     const [hospitals, setHospitals] = useState();
     const { state, city } = useParams();
     useEffect(() => {
-        const getHospitals = async() => {
+        const getHospitals = async () => {
             const url = `https://meddata-backend.onrender.com/data?state=${state}&city=${city}`;
             try {
                 const res = await axios.get(url);
@@ -19,12 +20,18 @@ const Hospitals = () => {
         }
         getHospitals();
     }, [])
-    return <div>
-        <Navbar bgColor={"white"}/>
-        <Box sx={{background: 'linear-gradient(91.75deg, #2AA7FF 1.4%, #0C8CE6 100.57%)', height:'110px', borderRadius:'0px 0px 16px 16px'}}>
-        
+    return <Box>
+        <Navbar bgColor={"white"} />
+        <Box sx={{ position: 'relative' }}>
+            <Box sx={{ background: 'linear-gradient(91.75deg, #2AA7FF 1.4%, #0C8CE6 100.57%)', height: '110px', borderRadius: '0px 0px 16px 16px' }}></Box>
+            <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
+                <Box sx={{position: 'absolute', backgroundColor: "white", borderRadius: '15px', boxShadow: '6px 6px 35px 0px #1028511C', width: '1300px' }}>
+                    <SearchComp padding={"0.6rem"} />
+                </Box>
+            </Box>
         </Box>
-    </div>
+    </Box>
 }
 
 export default Hospitals;
+
